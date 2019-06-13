dc35cd9fae9bf201064382debc8ca192e00fc81c
Justin Young <justin.kai.young@gmail.com>
2019-06-13 10:59:36 -0700

    Update chipotle_burrito_bot_script.py

--- chipotle_burrito_bot_script.py
+++ chipotle_burrito_bot_script.py
@@ -7,7 +7,7 @@ cache = []
 
 def grab_burrito_code(api):
     #grab most recent status
-    statuses = api.user_timeline(id='JustinY1998',count=5)
+    statuses = api.user_timeline(id='ChipotleTweets',count=5)
     most_recent_status = statuses[0].text
 
     #if we have already encountered this tweet do nothing
@@ -26,14 +26,14 @@ def grab_burrito_code(api):
     return code
 
 def text_to_number(code):
-    account_sid = 'AC9b701d6b1f21883b1fecf80871887afe'
-    auth_token = '57225797a6524c537baf51613eda4582'
+    account_sid = 'ACCOUNT_SID_HERE'
+    auth_token = 'ACCOUNT_TOKEN_HERE'
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                 .create(
                      body=code,
-                     from_='+16504762859',
+                     from_='+TWILIO_PHONE_NUMBER_HERE',
                      to='+888222'
                  )
     print("Message sent to Chipotle!")
